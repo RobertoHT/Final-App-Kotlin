87b8c171fda5d8d0ab10dd6828585fc4e40ae300
Roberto Hernández Tamayo <roberto.htamayo@gmail.com>
2018-06-23 00:00:47 -0500

    Feature - Recursos, vista login, create y forgot
    -Se agregaron los recursos a utilizar en el proyecto: strings, styles, dimen, colors
    -Se creo la vista Login
    -Se creo la vista Create account
    -Se creo la vista Forgot my password

---
+++ app/src/main/java/com/roberto/app/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.roberto.app.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.roberto.app.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
